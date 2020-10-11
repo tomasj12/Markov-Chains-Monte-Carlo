4cb51aba6a5900b0f02f1a3c9d028e56ef65f8f5
tomasj12 <tomasjurica1997@gmail.com>
2020-10-11 13:18:17 +0200

    Fix variable names, and add hist(Z) at the end of the script

--- Assignment 1/assignment_1_simulate_two_u_ratios.R
+++ Assignment 1/assignment_1_simulate_two_u_ratios.R
@@ -1,20 +1,22 @@
 #Assignment I
 n <- 1000
-X <- numeric(length = n)
+Z <- numeric(length = n)
 
 
 for(i in 1:1000) {
   
-  u1 <- runif(1,0,sqrt(1))
-  u2 <- runif(1,-10000,10000)
-  z <- u2/u1
-  while( u1^2 + u2^2 <=1) {
+  X <- runif(1,0,sqrt(1))
+  Y <- runif(1,-200,200)
+  z <- Y/X
+  while( X^2 > 1/(1 + z^2)) {
     
-    u1 <- runif(1,0,sqrt(1))
-    u2 <- runif(1,-10000,10000)
-    z <- u2/u1
+    X <- runif(1,0,sqrt(1))
+    Y <- runif(1,-200,200)
+    z <- Y/X
   }
   
-  X[i] <- z
+  Z[i] <- z
   print(paste0('Iteration: ',i,' is done.'))
 }
+
+hist(Z)
