f9f48ec6a0e2d3319345d17f4e1d84e9970c0efe
tomasj12 <tomasjurica1997@gmail.com>
2020-10-11 13:04:47 +0200

    Change uniform generating from the correct uniform in the case of random variable Y

--- Assignment 1/assignment_1_simulate_two_u_ratios.R
+++ Assignment 1/assignment_1_simulate_two_u_ratios.R
@@ -6,12 +6,12 @@ X <- numeric(length = n)
 for(i in 1:1000) {
   
   u1 <- runif(1,0,sqrt(1))
-  u2 <- runif(1,-1,1)
+  u2 <- runif(1,-10000,10000)
   z <- u2/u1
-  while( u1^2 * (1 + z^2) <=1) {
+  while( u1^2 + u2^2 <=1) {
     
     u1 <- runif(1,0,sqrt(1))
-    u2 <- runif(1,-1,1)
+    u2 <- runif(1,-10000,10000)
     z <- u2/u1
   }
   
